87053c69849894f5f8ab99c8cbf666c2507cf7c3
Qazzzie <santangelozach@gmail.com>
2021-07-02 16:32:59 -0700

    Carousel first commit, showing kittens

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Carousel from './components/carousel';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+  <Carousel/>
     </div>
   );
 }
