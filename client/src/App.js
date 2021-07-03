2bdfff9e06d118c5210f89eb8659efc61e44f7d1
Qazzzie <santangelozach@gmail.com>
2021-07-02 20:10:46 -0700

    Revert "Merge remote-tracking branch 'refs/remotes/origin/main' into main"
    This reverts commit 458d908621f871a2d97a48652add058ae28d385f, reversing
    changes made to 375c27d499c48aa73a86f38156e0d0f7c1add87d.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,3 @@
-import React, { useState } from 'react';
-import SelectGame from './components/SelectGame';
 import './App.css';
 import Carousel from './components/carousel';
 import GameRoom from './components/GameRoom';
@@ -13,27 +11,10 @@ const gameInfo = {
 const playerInfo = ["Mike", "Vandana", "Zach", "David", "QuizMASTER"]
 
 function App() {
-  const [listofGames, setListofGames] = useState([
-    {gameId: 1, gameName: "game 0", minPlayers: 4},
-    {gameId: 2, gameName: "game 1", minPlayers: 3},
-    {gameId: 3, gameName: "game 2", minPlayers: 2},
-    {gameId: 4, gameName: "game 4", minPlayers: 1},
-  ]);
-  const [selectedGame, setSelectedGame] = useState({
-    gameId: 0,
-    gameName: "Game Name",
-    minPlayers: "Min Players",
-  });
-
-  const handleSelectedGame = (gameName) => {
-    console.log("selected game = ", gameName);
-    setSelectedGame(gameName);
-  }
-
   return (
-    <div className="App font-mono">
+    <div className="App">
+      <h1>Hello world!</h1>
       <Carousel/>
-      <SelectGame handleSelectedGame={handleSelectedGame} listofGames={listofGames} />
       <GameRoom gameInfo={gameInfo} playerInfo={playerInfo}/>
     </div>
   );
