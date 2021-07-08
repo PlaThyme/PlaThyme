31c7b353541a46318e6a6e4f763910f345ce0282
Sree-Vandana <sreevandana.sv@gmail.com>
2021-07-08 13:17:37 -0700

    event listners for mouse actions (mouse up, mouse down, move and mouse click) working properly. now we can chose multiple colors

--- client/src/Games/DrawTheWord/board2.jsx
+++ client/src/Games/DrawTheWord/board2.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 import "./WhiteBoardContainer.css";
 
 export default function Board2() {
   const [timeoutValue, setTimeoutValue] = useState(undefined);
+  const colorsRef = useRef(null);
   const socket = io.connect("http://localhost:3001");
 
   useEffect(() => {
@@ -33,9 +34,50 @@ export default function Board2() {
     var sketch_style = getComputedStyle(sketch);
     canvas.width = parseInt(sketch_style.getPropertyValue("width"));
     canvas.height = parseInt(sketch_style.getPropertyValue("height"));
-
     var mouse = { x: 0, y: 0 };
     var last_mouse = { x: 0, y: 0 };
+    const colors = document.getElementsByClassName("color");
+
+    var lineWidthValue = 5;
+    var strokeColor = "blue";
+
+    // helper that will update the current color
+    const onColorUpdate = (e) => {
+      strokeColor = e.target.className.split(" ")[1];
+    };
+
+    // loop through the color elements and add the click event listeners
+    for (let i = 0; i < colors.length; i++) {
+      colors[i].addEventListener("click", onColorUpdate, false);
+    }
+
+    const onPaint = () => {
+      ctx.beginPath();
+      ctx.moveTo(last_mouse.x, last_mouse.y);
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.lineWidth = lineWidthValue;
+      ctx.strokeStyle = strokeColor;
+      ctx.closePath();
+      ctx.stroke();
+
+      if (timeoutValue !== undefined) {
+        clearTimeout(timeoutValue);
+      }
+      setTimeoutValue(
+        setTimeout(() => {
+          var base64ImageData = canvas.toDataURL("image/png"); // contains canvas images in coded fromat
+          socket.emit("canvas-data", base64ImageData);
+        }, 1000)
+      );
+    };
+
+    // const onMouseDown = (e) => {
+    //   onPaint("from mouseDown");
+    // };
+
+    // const onMouseUp = (e) => {
+    //   onPaint("from mouse Up");
+    // };
 
     /* Mouse Capturing Work */
     canvas.addEventListener(
@@ -51,10 +93,10 @@ export default function Board2() {
     );
 
     /* Drawing on Paint App */
-    ctx.lineWidth = 5;
+    // ctx.lineWidth = 5;
     ctx.lineJoin = "round";
     ctx.lineCap = "round";
-    ctx.strokeStyle = "blue";
+    // ctx.strokeStyle = "blue";
 
     canvas.addEventListener(
       "mousedown",
@@ -71,33 +113,23 @@ export default function Board2() {
       },
       false
     );
-
-    var onPaint = function () {
-      ctx.beginPath();
-      ctx.moveTo(last_mouse.x, last_mouse.y);
-      ctx.lineTo(mouse.x, mouse.y);
-      ctx.closePath();
-      ctx.stroke();
-
-      if (timeoutValue !== undefined) {
-        clearTimeout(timeoutValue);
-      }
-      setTimeoutValue(
-        setTimeout(() => {
-          var base64ImageData = canvas.toDataURL("image/png"); // contains canvas images in coded fromat
-          socket.emit("canvas-data", base64ImageData);
-        }, 1000)
-      );
-    };
-  };
+  };;
 
   return (
     <div className="container">
       <div className="board-container sketch mt-8" id="sketch">
         <canvas id="board" className="board" />
       </div>
-      <div className="color-picker-contsiner">
+      {/* <div className="color-picker-contsiner">
         <input type="color" />
+      </div> */}
+
+      <div ref={colorsRef} className="colors">
+        <div className="color black" />
+        <div className="color red" />
+        <div className="color green" />
+        <div className="color blue" />
+        <div className="color yellow" />
       </div>
     </div>
   );
