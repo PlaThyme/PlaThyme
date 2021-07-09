d228ad69877758626f8a117f95afca7582bcf9c8
Sree-Vandana <sreevandana.sv@gmail.com>
2021-07-09 15:31:50 -0700

    added icons with correct css

--- client/src/Games/DrawTheWord/DrawingBoard.jsx
+++ client/src/Games/DrawTheWord/DrawingBoard.jsx
@@ -115,7 +115,7 @@ export default function DrawingBoard(props) {
   }, []);
 
   return (
-    <div className="grid-container mt-10">
+    <div className="grid-container mt-20">
       <div className="grid-item item-1 text-white">
         <p>Timer: 1:29</p>
       </div>
@@ -125,27 +125,64 @@ export default function DrawingBoard(props) {
       <div className="board-container sketch grid-item item-3" id="sketch">
         <canvas id="board" className="board" />
       </div>
-      <div ref={colorsRef} className="colors grid-item item-4">
-        <div className="color black odd" />
-        <div className="color white  even" />
-        <div className="color darkGray odd" />
-        <div className="color lightGray even" />
-        <div className="color maroon odd" />
-        <div className="color brown even" />
-        <div className="color red odd" />
-        <div className="color lightpink even" />
-        <div className="color orange odd" />
-        <div className="color lightOrange even" />
-        <div className="color yellow odd" />
-        <div className="color lightYellow even" />
-        <div className="color green odd" />
-        <div className="color lightGreen even" />
-        <div className="color blue odd" />
-        <div className="color lightBlue even" />
-        <div className="color indigo odd" />
-        <div className="color lightIndigo even" />
-        <div className="color purple odd" />
-        <div className="color lightPurple even" />
+      <div className="grid-item item-4 flex justify-between">
+        <div ref={colorsRef} className="colors">
+          <div className="color black odd" />
+          <div className="color white  even" />
+          <div className="color darkGray odd" />
+          <div className="color lightGray even" />
+          <div className="color maroon odd" />
+          <div className="color brown even" />
+          <div className="color red odd" />
+          <div className="color lightpink even" />
+          <div className="color orange odd" />
+          <div className="color lightOrange even" />
+          <div className="color yellow odd" />
+          <div className="color lightYellow even" />
+          <div className="color green odd" />
+          <div className="color lightGreen even" />
+          <div className="color blue odd" />
+          <div className="color lightBlue even" />
+          <div className="color indigo odd" />
+          <div className="color lightIndigo even" />
+          <div className="color purple odd" />
+          <div className="color lightPurple even" />
+        </div>
+        <div className="canvasTools flex justify-items-end">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-pencil-fill"
+            viewBox="0 0 16 16"
+            className="color yellow"
+          >
+            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-eraser-fill"
+            viewBox="0 0 16 16"
+            className="color yellow"
+          >
+            <path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm.66 11.34L3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-trash-fill"
+            viewBox="0 0 16 16"
+            className="color yellow"
+          >
+            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+          </svg>
+        </div>
       </div>
     </div>
   );
