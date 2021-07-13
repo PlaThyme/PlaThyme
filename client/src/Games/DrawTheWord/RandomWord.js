8c8078a0e9e45605b9904fea4fd4186a053a61b6
Qazzzie <santangelozach@gmail.com>
2021-07-12 22:26:24 -0700

    word selector and popup react state component

--- client/src/Games/DrawTheWord/RandomWord.js
+++ client/src/Games/DrawTheWord/RandomWord.js
@@ -18,69 +18,16 @@ const RandomWord = () => {
     hardwords.splice(randint);
     console.log(ret[0]);
 
-    let [isOpen, setIsOpen] = useState(true);
-    let [selectedWord, setSelectedWord] = useState('');
-    isOpen = true;
-
-    
     return (
-<>
-    {/* <Transition appear open={isOpen}> */}
-    <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" open={isOpen} onClose={() => setIsOpen(false)}>
-        <div className="min-h-screen px-4 text-center"></div>
-        <Dialog.Overlay className="fixed inset-0" />
-
-    {/* This element is to trick the browser into centering the modal contents. */}
-    <span
-    className="inline-block h-screen align-middle"
-    aria-hidden="true"
-    >
-    &#8203;
-    </span>
-    {/* <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-200"
-              enterFrom="opacity-0 scale-50"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-50"
-            > */}
-
-   <div className="inline-block max-w-md p-4 my-5 overflow-hidden text-left align-middle transition-all transform bg-thyme-700 shadow-md rounded-lg">
-
-        <Dialog.Title  
-        as="h3"
-        className="text-lg font-medium leading-6 text-thyme-300">Choose the word you draw!
-        </Dialog.Title>
-        
 
 
-        <div>
-            <button 
-            type="button" 
-            className="max-w-full px-4 py-2 text-sm font-medium shadow-md text-thyme-900 bg-thyme-100 rounded-md hover:bg-thyme-200" 
-            onclick={() => setSelectedWord(ret[0])}>
-                {ret[0]}
-            </button>
-        <br></br>
-            <button 
-            type="button" 
-            className="px-4 py-2 text-sm font-medium shadow-md text-thyme-900 bg-thyme-100 rounded-md hover:bg-thyme-200" 
-            onclick={() => setSelectedWord(ret[1])} >{ret[1]}</button>
-        <br></br>
-            <button 
-            type="button" 
-            className="px-4 py-2 text-sm font-medium shadow-md text-thyme-900 bg-thyme-100 rounded-md hover:bg-thyme-200"             
-            onclick={() => setSelectedWord(ret[2])} >{ret[2]}</button>
-        </div>
-    
+    <div>
+        <ul>
+        <button>{ret[0]}</button>
+        <button>{ret[0]}</button>
+        <button>{ret[0]}</button>
+        </ul>
     </div>
-    
-    </Dialog>
-    {/* </Transition.Child> */}
-    {/* </Transition>  */}
-</>
     )
 }
 
