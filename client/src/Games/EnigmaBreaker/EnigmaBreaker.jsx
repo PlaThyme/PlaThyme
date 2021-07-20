fe6dbee99969eeca71e2aceaa41089516da8a002
dwareb <zahthris@gmail.com>
2021-07-20 14:23:10 -0700

    Initial creation

---
+++ client/src/Games/EnigmaBreaker/EnigmaBreaker.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./EnigmaBreakerStyle.css";
+
+const EnigmaBreaker = () => {
+    return (
+        <div className="enigma-grid">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione at animi aspernatur, temporibus iste sint obcaecati ullam dolorem iusto possimus. Adipisci explicabo officiis assumenda? Assumenda, maiores inventore. Ipsam, id nam.</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, et! Maiores, aliquam. Nesciunt natus hic ex beatae similique dolorum ea sapiente, fuga minus, pariatur, autem eligendi amet eius quidem velit?</div>
+            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque, praesentium? Deleniti, velit ex optio facere laborum voluptas neque commodi culpa exercitationem fuga reiciendis, inventore autem iure quos laboriosam, magni fugit.</div>
+            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus incidunt harum cum iste. Sequi eveniet quod reiciendis eum ducimus perferendis! Id perspiciatis commodi voluptatibus vel, veniam ad numquam dolores eos!</div>
+            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi quidem non hic, reprehenderit qui laborum beatae eos molestias fugiat praesentium sint reiciendis voluptate natus quibusdam unde harum, facilis laboriosam numquam.</div>
+        </div>
+    )
+}
+
+export default EnigmaBreaker
